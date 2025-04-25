b2b844315ea1d8c3ceb941285a8ffd7b332190a1
EngineerDev11 <EngineerDev11@outlook.com>
2025-04-25 14:11:45 +0530

    Implement ScrollableTabs in MapControlIcons; add state management for tab visibility

--- src/components/ListenPage/Map/MapControlIcons.tsx
+++ src/components/ListenPage/Map/MapControlIcons.tsx
@@ -2,36 +2,46 @@ import { Box, Fab, Stack } from '@mui/material';
 import { useRoundware } from '@/hooks';
 import RoundwareMixerControl from '../RoundwareMixerControl';
 import infoIcon from '@/assets/icons/info_i_icon.svg';
+import { useState } from 'react';
+import ScrollableTabs from '@/components/ScrollableTabs';
 
 const MapControlIcons = () => {
   const { roundware } = useRoundware();
+  const [showTabs, setShowTabs] = useState(false);
+
+  const handleCloseTabs = () => {
+    setShowTabs(false);
+  };
 
   return (
-    <Stack
-      spacing={2}
-      position="absolute"
-      top={20}
-      right={20}
-      zIndex={1}
-    >
-      <Fab color="secondary" size="medium">
-        <Box
-          component="img"
-          src={infoIcon}
-          alt="Info"
-          sx={{
-            height: 24,
-            width: 24
-          }}
-        />
-      </Fab>
-      <Fab 
-        color={roundware.mixer?.playing ? "primary" : "secondary"} 
-        size="medium"
+    <Box>
+      <Stack
+        spacing={2}
+        position="absolute"
+        top={20}
+        right={20}
+        zIndex={1200}
       >
-        <RoundwareMixerControl />
-      </Fab>
-    </Stack>
+        <Fab color="secondary" size="medium" onClick={() => setShowTabs(true)}>
+          <Box
+            component="img"
+            src={infoIcon}
+            alt="Info"
+            sx={{
+              height: 24,
+              width: 24
+            }}
+          />
+        </Fab>
+        <Fab 
+          color={roundware.mixer?.playing ? "primary" : "secondary"} 
+          size="medium"
+        >
+          <RoundwareMixerControl />
+        </Fab>
+      </Stack>
+      <ScrollableTabs open={showTabs} onClose={handleCloseTabs} />
+    </Box>
   );
 };
 
