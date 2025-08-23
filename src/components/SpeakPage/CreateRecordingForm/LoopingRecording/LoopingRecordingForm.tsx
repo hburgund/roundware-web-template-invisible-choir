86cb25f2144306149d07a49f8fb254786afef55e
EngineerDev11 <EngineerDev11@outlook.com>
2025-08-23 23:17:51 +0530

    Remove unused popstate event

--- src/components/SpeakPage/CreateRecordingForm/LoopingRecording/LoopingRecordingForm.tsx
+++ src/components/SpeakPage/CreateRecordingForm/LoopingRecording/LoopingRecordingForm.tsx
@@ -73,21 +73,8 @@ const LoopingRecordingForm = () => {
 
   // Cleanup on unmount & back button handling
   useEffect(() => {
-    // const handlePopState = () => {
-    //   if (showJoinChoirPage) {
-    //     // On join choir page → go back to listen
-    //     cleanupRecordingSession();
-    //     history.push("/listen", { source: 'recording' });
-    //   } else {
-    //     // On recording page → go back to join choir
-    //     setShowJoinChoirPage(true);
-    //   }
-    // };
-
-    // window.addEventListener('popstate', handlePopState);
     
     return () => {
-      // window.removeEventListener('popstate', handlePopState);
       console.log("🧹 LoopingRecordingForm unmounting - cleaning up");
       cleanupRecordingSession();
     };
